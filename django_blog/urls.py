6703973ba6f95d852762a0e29bdc530ffd42f023
cthoms1993 <50752438+cthoms1993@users.noreply.github.com>
2020-04-23 17:02:41 +0100

    added

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -13,15 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.conf.urls import url, include
 from django.views.generic import RedirectView
 from django.views.static import serve
 from .settings import MEDIA_ROOT
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', RedirectView.as_view(url='posts/')),
     url(r'^posts/', include('posts.urls')),
     url(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT})
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
